fe5fcfda307f0d39f91a19831789f060a829fcf8
Miracle Okeke <mokeke185@gmail.com>
2024-01-15 15:13:20 +0100

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,13 @@
-import Head from "next/head";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "EliteAndAce Properties",
+  description: "To be a Leading Real Estate Agent in Sub Saharan Africa ",
+};
 
 export default function RootLayout({
   children,
@@ -12,19 +16,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        {/* Add more meta tags as needed */}
-        <meta name="theme-color" content="#ffffff" />
-        {/* Include your custom metadata */}
-        <title>EliteAndAce Properties</title>
-        <meta
-          name="description"
-          content="To be a Leading Real Estate Agent in Sub Saharan Africa"
-        />
-        {/* Add more meta tags based on your requirements */}
-        <link rel="icon" type="image/svg+xml" href="/favicon.ico" />
-      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
