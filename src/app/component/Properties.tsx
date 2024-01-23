1694684f544c47217dcbba24683b851b5edd15a5
Miracle Okeke <mokeke185@gmail.com>
2024-01-23 02:06:28 +0100

    Contact Added

--- src/app/component/Properties.tsx
+++ src/app/component/Properties.tsx
@@ -32,7 +32,7 @@ const Properties = () => {
 
           <div className="md:max-w-[500px]  relative">
             <Image
-              src="/design2.svg"
+              src="/design11.svg"
               alt="List"
               width={100}
               height={100}
@@ -41,14 +41,14 @@ const Properties = () => {
 
             <div className="bg-white rounded-b-md shadow-lg shadow-[#ccc] py-4 px-4 flex flex-col items-center gap-5 relative">
               <button className="bg-[#052087]  px-6 py-3 font-bold rounded-md text-white font-Montserrat text-[9px] max-w-[180px] absolute  top-[1%]  -translate-y-1/2 left-1/2 transform -translate-x-1/2">
-                Terrace
+                Duplex
               </button>
             </div>
           </div>
 
           <div className="md:max-w-[500px]  relative">
             <Image
-              src="/design6.svg"
+              src="/design9.svg"
               alt="List"
               width={100}
               height={100}
@@ -57,7 +57,7 @@ const Properties = () => {
 
             <div className="bg-white rounded-b-md shadow-lg shadow-[#ccc] py-4 px-4 flex flex-col items-center gap-5 relative">
               <button className="bg-[#052087]  px-6 py-3 font-bold rounded-md text-white font-Montserrat text-[9px] max-w-[180px] absolute  top-[1%]  -translate-y-1/2 left-1/2 transform -translate-x-1/2">
-                Terrace
+                Bungalow
               </button>
             </div>
           </div>
