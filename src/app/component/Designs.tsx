02933be32777f8dac4891e0f2dbd6a0ce0384d7c
thegodhope <103210115+thegodhope@users.noreply.github.com>
2024-02-13 16:05:26 +0100

    made changes to the components

--- src/app/component/Designs.tsx
+++ src/app/component/Designs.tsx
@@ -75,6 +75,17 @@ const Design = () => {
               </button>
             </div>
           </div>
+          <div className="md:max-w-[500px] rounded-md  relative">
+            <Image src={Fruit3} alt="List" className="w-full rounded-t-md" />
+            <div className="bg-[#052087] w-full h-1"></div>
+            <div className="bg-white rounded-b-md shadow-lg shadow-[#ccc] py-4 px-4 flex flex-col items-center gap-5 relative">
+              <h4 className="text-[15px] font-medium text-center">Duplex</h4>
+
+              <button className="bg-[#052087] hover:bg-[#ee5457] px-6 py-2  font-bold rounded-full text-white font-Montserrat text-[7px]  max-w-[180px]">
+                <Link href="/Contact">Contact Us</Link>
+              </button>
+            </div>
+          </div>
           <div className="md:max-w-[500px]  rounded-md  relative">
             <Image src={Fruit6} alt="List" className="w-full  rounded-t-md" />
             <div className="bg-[#052087] w-full h-1"></div>
@@ -89,6 +100,17 @@ const Design = () => {
 
           <div className="md:max-w-[500px] rounded-md  relative">
             <Image src={Fruit5} alt="List" className="w-full rounded-t-md" />
+            <div className="bg-[#052087] w-full h-1"></div>
+            <div className="bg-white rounded-b-md shadow-lg shadow-[#ccc] py-4 px-4 flex flex-col items-center gap-5 relative">
+              <h4 className="text-[15px] font-medium text-center">Bungalow</h4>
+
+              <button className="bg-[#052087] hover:bg-[#ee5457] px-6 py-2  font-bold rounded-full text-white font-Montserrat text-[7px]  max-w-[180px]">
+                <Link href="/Contact">Contact Us</Link>
+              </button>
+            </div>
+          </div>
+          <div className="md:max-w-[500px] rounded-md  relative">
+            <Image src={Fruit7} alt="List" className="w-full rounded-t-md" />
             <div className="bg-[#052087] w-full h-1"></div>
             <div className="bg-white rounded-b-md shadow-lg shadow-[#ccc] py-4 px-4 flex flex-col items-center gap-5 relative">
               <h4 className="text-[15px] font-medium text-center">Bungalow</h4>
